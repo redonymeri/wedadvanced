f4b31df8f0afc0367562216bc3feb980f3f2af1a
redonymeri <ymeriredon2@gmail.com>
2026-03-05 17:41:13 +0100

    moduli 26

--- moduli 26/main.js
+++ moduli 26/main.js
@@ -1,1 +1,60 @@
 console.log("this is my first learning JavaScript");
+
+var y = 10;
+
+var consoleMessage = "this is a console";
+
+var x = 40;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+var a = 99;
+a++;
+console.log(a);
+
+var b = 99; 
+b--;
+console.log(b);
+
+
+var c = 67;
+c += 10;
+console.log(c);
+
+
+var d = 96;
+d -= 5;
+console.log(d);
+
+
+var e = 5;
+e *= 10;
+console.log(e);
+
+
+var f = 100;
+f /= 2;
+console.log(f);
+
+g = 100;
+
+h = 50;
+
+console.log(g = h);
+console.log(g == h);
+console.log(g > h);
+console.log(g < h);
+
+
+var name = "redon";
+var lastname = "hoxha";
+
+var fullname = name + " " + lastname;
+
+console.log(fullname);
+
+console.log(`hello to ${fullname}`);
+
