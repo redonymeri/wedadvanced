339364fd6663bc664230415c6fc33bccbdea3d00
redonymeri <ymeriredon2@gmail.com>
2026-03-10 17:44:26 +0100

    moduli 27

---
+++ moduli 27/script.js
@@ -0,0 +1,11 @@
+function displayAlert() {
+    alert("using alert with functions");
+}
+
+displayAlert();
+
+function mbledja(numri1, numri2) {
+    return numri1 + numri2;
+}
+
+document.write(mbledja(5, 10));
