11136ef10718699878ccd5afc89e9d3ccb0ee94e
redonymeri <ymeriredon2@gmail.com>
2026-03-17 17:49:03 +0100

    moduli 28

---
+++ moduli 28/script.js
@@ -0,0 +1,25 @@
+var button2 =document.getElementById("btn2");
+button2.onclick = function (){
+    alert('kuku cka ke fitu ti')
+}
+
+var button1 =document.getElementById("btn1");
+button1.onclick = function (){
+    alert('cka bone ti')
+}
+
+var button3 =document.getElementById("btn3");
+button3.onmouseover = function (){
+    alert('kuku cka ke fitu ti')
+}
+
+var button4 =document.getElementById("btn4");
+button4.onmousewheel = function (){
+    alert('cka bone ti')
+}
+
+var v_button = document.getElementById("btn6")
+var v_text = document.getElementById("txt1")
+v_button.onclick = function(){
+    v_text.style.color='red'
+}
