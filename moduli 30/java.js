0b66437a77e492a67036879fac7388fb61c57633
redonymeri <ymeriredon2@gmail.com>
2026-03-26 17:45:42 +0100

    moduli 30

---
+++ moduli 30/java.js
@@ -0,0 +1,17 @@
+//var mosha = 7;
+
+//if(mosha < 8) {
+    //document.write("your child isw to young acess declined")
+//} else {
+    //document.write("your child is old egnouf to acess")
+//}
+
+var age=6;
+
+if(age<19){
+    document.write("your child is to young");
+} else if(age>18){
+    document.write("your child is to old egnouf");
+} else{
+    document.write("we will call you soon to let y know for the groups");
+}
