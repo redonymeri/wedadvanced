5a4e984811aeab74330aec07a7128512b90db2db
redonymeri <ymeriredon2@gmail.com>
2026-03-26 17:30:30 +0100

    moduli 29

---
+++ moduli 29/java.js
@@ -0,0 +1,68 @@
+var programingLaunguages = ["javascript" , "PHP", "Python"]
+
+console.log(programingLaunguages);
+
+var numrat = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(numrat);
+
+console.log(programingLaunguages);
+
+
+programingLaunguages.push("Java");
+
+numrat.push(11);
+
+document.write(programingLaunguages);
+
+document.write(numrat);
+
+//pop delete the last element
+
+numrat.pop();
+
+console.log(numrat);
+
+//unshift 
+
+programingLaunguages.unshift("C#");
+
+console.log(programingLaunguages);
+
+programingLaunguages.splice(1, 2, "c++");
+
+console.log(programingLaunguages);
+
+var student = ["lindi", "patri", "Redo",]
+var [s1 , s2 , s3] = student;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+var places = ["podujev", "california", "newyork", "tokyo", "london", "paris", "madrit", "prishtine", "huston", "stocholm", "melborne", "shangai", "edinburgh", "tirana", "capetown", "mexico city", "seoul", "zurchi", 
+"Rio de janerio", "fushekosove", "copehagen"]
+
+var [s1 , s2 , s3 , s4 , s5 , s6 ,s7 , s8 , s9 s10 , s11 , s12 s13 , s14 , s15, s16 ,s17 ,s18 ,s19 ,s20 ,s21 ] = places;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(s5);
+console.log(s6);
+console.log(s7);
+console.log(s8);
+console.log(s9);
+console.log(s10);
+console.log(s11);
+console.log(s12);
+console.log(s13);
+console.log(s14);
+console.log(s15);
+console.log(s16);
+console.log(s17);
+console.log(s18);
+console.log(s19);
+console.log(s20);
+console.log(s21);
