b6516af8ee26f2e6f71e6a8cbe6fff118bc5f2b7
redonymeri <ymeriredon2@gmail.com>
2026-04-14 17:48:14 +0200

    moduli 30

--- moduli 30/java.js
+++ moduli 30/java.js
@@ -1,9 +1,48 @@
 var wakeuptime;
 var dstime;
-car sleeptime;
+var sleeptime;
 var noon = 12;
 
 
+
+
 function showCurrentTime(){
-    var
-}
+    var clock = document.getElementById('clock');
+    var currentTime = new Date();
+
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+
+    var meridian = "AM";
+
+
+
+    if(hours >= noon){
+        meridian = "PM";
+    }
+
+
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian;
+    clock.innerText = clockTime;
+    changeImage();
+};
+
+var oneSecond = 1000;
+setInterval(showCurrentTime .oneSecond);
+
+
+function changeImage()[
+    var time = new Date(). getHours();
+
+    var images = "img/ds_clock.png"
+    var imageHTML = document.getElementById("timeImage")
+
+    if(time == wakeuptime){
+        images = "img/moring.gif";
+    }
+
+    
+]
